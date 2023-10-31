c5e22a6350620ede82f9a993528bc91f245fd648
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 14:54:43 +0900

    jsx에서 js코드 작성은 {} 사용 연습 2

--- src/App.js
+++ src/App.js
@@ -11,12 +11,15 @@ function MyComponent1() {
   const contry = "korea";
   const phone = "010";
   const age = 33;
+  const randomNumber = Math.random();
   return (
     <>
       <h1>age : {age}</h1>
       <h1>address : {address}</h1>
       <h1>contry : {contry}</h1>
       <h1>phone : {phone}</h1>
+      <h1>random : {randomNumber}</h1>
+      <h1>1~10 : {Math.ceil(randomNumber * 10)}</h1>
     </>
   );
 }
