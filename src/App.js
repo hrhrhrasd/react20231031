5ddb452d230162627f1a4e1a078099cdaff0c85c
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 16:33:41 +0900

    인라인 스타일 적용 연습 (display: flex)사용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-
 function App() {
+  const myStyle = {
+    width: "400px",
+    display: "flex",
+    justifyContent: "space-between",
+  };
   return (
-    <div>
-
+    <div style={myStyle}>
+      <MyComp />
+      <MyComp />
+      <MyComp />
     </div>
   );
+}
+
+function MyComp() {
+  const myStyle2 = {
+    width: "50px",
+    backgroundColor: "black",
+    color: "white",
+    textAlign: "center",
+    borderRadius: "10px",
+  };
+  const name = "흥민";
+  return <div style={myStyle2}>{name}</div>;
 }
 
 export default App;
