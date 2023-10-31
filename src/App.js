98e9c5df25acad32da3a105fa10cd48304c221f3
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 16:19:14 +0900

    인라인 스타일 적용하는 방법

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 function App() {
-  const num1 = Math.ceil(Math.random() * 6);
-  const num2 = Math.ceil(Math.random() * 6);
-
-  let messageClassName = num1 === num2 ? "win" : "lose";
-  let message = num1 === num2 ? "당첨" : "꽝";
+  const myStyle = {
+    color: "white",
+    backgroundColor: "black", // lowerCamelCase
+    fontSize: "70px",
+    textAlign: "center",
+  }; //js object
 
   return (
     <>
-      <h1>1번 주사위 {num1}</h1>
-      <h1>1번 주사위 {num2}</h1>
-      <h1 className={messageClassName}>{message}</h1>
+      {/* style 속성은 객체로 주어야 함 */}
+      <div style={myStyle}>Lorem ipsum dolor.</div>
+      <br />
+      <div
+        style={{
+          color: "white",
+          backgroundColor: "black",
+          fontSize: "70px",
+          textAlign: "center",
+        }}
+      >
+        Lorem ipsum dolor.
+      </div>
     </>
   );
 }
