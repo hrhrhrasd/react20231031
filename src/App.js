084af3e665dc35ec47b148855aa0af1d320b25e0
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 11:55:18 +0900

    componenet prac1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@
 function App() {
   return (
     <div>
-
+        <MyComponent1 />
+        <MyComponent2 />
     </div>
   );
+}
+
+function MyComponent1() {
+    return (
+        <div>
+            <h1>Hello Component</h1>
+        </div>
+    );
+}
+
+function MyComponent2() {
+    return (
+        <>
+            <p>Lorem ipsum dolor sit amet.</p>
+            <ul>
+                <li>Lorem.</li>
+                <li>Quo.</li>
+                <li>Illum.</li>
+            </ul>
+        </>
+    )
 }
 
 export default App;
