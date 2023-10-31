f543feff87284c4d9bbc8c3d935817c0e50d2e37
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 14:49:13 +0900

    jsx에서 js코드 작성은 {} 사용 연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
 function App() {
   return (
     <div>
-
+      <MyComponent1 />
     </div>
+  );
+}
+
+function MyComponent1() {
+  const address = "seoul";
+  const contry = "korea";
+  const phone = "010";
+  const age = 33;
+  return (
+    <>
+      <h1>age : {age}</h1>
+      <h1>address : {address}</h1>
+      <h1>contry : {contry}</h1>
+      <h1>phone : {phone}</h1>
+    </>
   );
 }
 
