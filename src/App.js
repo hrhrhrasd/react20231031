87c55b56cc9b8af94e0a4222c6c89a2618bae6e5
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 15:34:27 +0900

    jsx {} 에서 간단한 연산자 활용 연습2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
 function App() {
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+  let message;
+
+  if (num1 === num2) {
+    message = <h1>당첨</h1>;
+  } else {
+    message = <h1>꽝</h1>;
+  }
+
   return (
     <div>
-
+      {num1 === num2 ? <h1>당첨</h1> : <h1>꽝</h1>}
+      {message}
     </div>
   );
 }
