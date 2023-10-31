03c237fc93fc618f0440df4722eb33f346783021
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 15:54:33 +0900

    프로퍼티의 값은 js코드가 될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 function App() {
-  const number = Math.ceil(Math.random() * 100); //1~100
-  const isLarge = number > 50;
-  const isSmall = number <= 50;
+  const num1 = Math.ceil(Math.random() * 6);
+  const num2 = Math.ceil(Math.random() * 6);
+
+  let messageClassName = num1 === num2 ? "win" : "lose";
+  let message = num1 === num2 ? "당첨" : "꽝";
 
   return (
-    <div>
-      <h1>난수 : {number}</h1>
-      <h1>{number > 50 ? "큰수" : "작은수"}</h1>
-      <div>{number > 50 ? <BigImage /> : <SmallImage />}</div>
-      <div>{isLarge && <h1>큰수</h1>}</div>
-      <div>{isLarge || <h3>작은수</h3>}</div>
-    </div>
+    <>
+      <h1>1번 주사위 {num1}</h1>
+      <h1>1번 주사위 {num2}</h1>
+      <h1 className={messageClassName}>{message}</h1>
+    </>
   );
 }
-
-function BigImage() {
-  return <h1>큰 이미지</h1>;
-}
-
-function SmallImage() {
-  return <h3>작은 이미지</h3>;
-}
-
 export default App;
