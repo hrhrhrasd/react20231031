21c3b7a994981751d2f5d1a29188b3729f78ac14
hrhrhrasd <gmleh1245@gmail.com>
2023-10-31 11:40:12 +0900

    component, 주석, jsx작성 방법

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 
-function App() {
-  return (
-    <div>
+function App() {    // component
+    return (
+        <div>
+            <h1>hello react</h1>
+            <h2>hi</h2>
+            <MyComp />
+        </div>
+);
+}
 
-    </div>
-  );
+// built-in component 는 소문자로 시작
+// component 명은 대문자로 시작
+// return 문을 가져야 함
+// return 값은 보통 jsx코드
+
+function MyComp() {
+    // return 에 jsx코드 작성 가능
+    // 꼭 하나의 root tag가 존재해야 함
+    // return 값이 여러줄이면 ()
+    // root tag 가 필요없다면 fragment 사용 가능 (tag명이 없는 tag)
+
+    // jsx 에서 주석 사용 : {/*   */} (ctrl + /)
+    return (
+        <>
+            <h1>hi comp</h1>
+            <h2>asdad</h2>
+        </>
+    );
 }
 
 export default App;
