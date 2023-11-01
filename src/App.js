01e04176ac70946113d5c1ca6dba7c2d33e00bf3
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 10:31:51 +0900

    props destructuring assignment 구조 분해 할당

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
 function App() {
   return (
     <>
-      <h1
-        className={"note"}
-        style={{
-          background: "blue",
-          color: "white",
-        }}
-        title={"제목요소"}
-      >
-        Lorem.
-      </h1>
-      <p title={"문단요소"}>Lorem ipsum dolor.</p>
-      <MyComp title={"myComp"} name={"흥민"} />
-      <MyComp title={"myComp2"} name={"강인"} />
+      <MyComp value={"흥민"} address={"신촌"} />
+      <MyComp value={"강인"} address={"강남"} />
     </>
   );
 }
 
-//  함수 인자로
-//  사용한 곳 에서 넘긴 property들이 객체형태로 담김
-function MyComp(props) {
-  console.log(props);
+function MyComp({ address, value }) {
   return (
     <div>
-      <h1 title={props.title} name={props.name}>
-        hello
+      <h1>
+        {value} : {address}거주
       </h1>
     </div>
   );
 }
+
 export default App;
