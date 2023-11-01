b9ceb95fa52c0f2553111d2b07c333bffb581f7b
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 10:46:08 +0900

    props 구조 분해 할당 연습 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-
 function App() {
   return (
     <div>
+      <MyElem name={"흥민"} age={44} team={"토트넘"} fcolor={"blue"} />
+      <MyElem name={"강인"} age={33} team={"파리"} fcolor={"red"} />
+    </div>
+  );
+}
 
+function MyElem({ name, age, team, fcolor }) {
+  return (
+    <div style={{ color: fcolor }}>
+      <h1>
+        name = {name} age = {age} team = {team}
+      </h1>
     </div>
   );
 }
