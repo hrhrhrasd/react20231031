240ca98fbb6e6b3a8886ecf7d143b32a3821af99
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 14:27:16 +0900

    default export, import 연습 1

---
+++ src/component/MyContainer.js
@@ -0,0 +1,19 @@
+function MyBox() {
+  return (
+    <div>
+      <h1>MyBox text</h1>
+    </div>
+  );
+}
+
+export const person = {
+  name: "son",
+  address: "seoul",
+};
+
+export const team = {
+  title: "soccer",
+  score: "5",
+};
+
+export default MyBox;
