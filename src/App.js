d5503d27e796ff635ee4545d7f467e8b96b41b4e
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 12:13:50 +0900

    다른 파일의 컴포넌트 export / import 연습 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { MyText } from "./component/import1";
+import { MyBox } from "./component/import1";
+import { MyContainer } from "./component/import1";
+import { MyNavbar } from "./component/import1";
 
 function App() {
   return (
     <div>
-
+      <MyText />
+      <MyBox />
+      <MyContainer />
+      <MyNavbar />
     </div>
   );
 }
---
+++ src/component/import1.js
@@ -0,0 +1,31 @@
+export function MyText() {
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export function MyBox() {
+  return (
+    <div>
+      <p>world</p>
+    </div>
+  );
+}
+
+export function MyContainer() {
+  return (
+    <div>
+      <p>react</p>
+    </div>
+  );
+}
+
+export function MyNavbar() {
+  return (
+    <div>
+      <p>import</p>
+    </div>
+  );
+}
