4eff6f970b7c13c0a908ae13b919d4b3ff71d37b
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 12:32:43 +0900

    다른 파일의 컴포넌트 같은 파일이라면 ,로 구분 import 연습 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+import {
+  country,
+  MyContent,
+  MyElem,
+  MyNavBar,
+  text,
+  value,
+} from "./component/import1";
 
 function App() {
   return (
     <div>
-
+      <p>{country}</p>
+      <p>{text}</p>
+      <p>{value}</p>
+      <MyElem />
+      <MyNavBar />
+      <MyContent />
     </div>
   );
 }
---
+++ src/component/import1.js
@@ -0,0 +1,13 @@
+export function MyElem() {
+  return <div>MyElem</div>;
+}
+export function MyNavBar() {
+  return <div>MyNavBar</div>;
+}
+export function MyContent() {
+  return <div>MyContent</div>;
+}
+
+export const country = "korea";
+export const text = "text1";
+export const value = [10, 20, 30];
