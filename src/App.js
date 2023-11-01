aae3dff881b016698bed5e027f5ab6b3e7d81341
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 11:35:30 +0900

    props 구조 분해 할당 연습 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
-
 function App() {
   return (
     <div>
+      <MyComp
+        m={"30px"}
+        p={"10px"}
+        color={"gold"}
+        bg={"black"}
+        text={"hello react"}
+      />
+      <MyComp m={"30px"} p={"10px"} color={"gold"} bg={"black"} />
+      <MyComp p={"10px"} color={"gold"} text={"hello react"} />
+      <MyComp m={"30px"} bg={"black"} text={"hello react"} />
+    </div>
+  );
+}
 
+function MyComp({
+  m = "20px",
+  p = "30px",
+  color = "green",
+  bg = "red",
+  text = "hello world",
+}) {
+  return (
+    <div
+      style={{
+        margin: m,
+        padding: p,
+        color: color,
+        background: bg,
+      }}
+    >
+      <p>{text}</p>
     </div>
   );
 }
