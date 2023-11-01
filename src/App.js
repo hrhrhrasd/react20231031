78ee66fb448194d547b9690c2af0f0ba213ade3c
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 09:46:21 +0900

    props 연습 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
+function MyHeader(props) {
+  return (
+    <div
+      style={{
+        color: props.color,
+        background: props.bg,
+      }}
+    >
+      <h1>{props.text}</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
-
+      <MyHeader color={"white"} bg={"black"} text={"내가 만든 제목"} />
+      <MyHeader color={"orange"} bg={"skyblue"} text={"다시 만든 제목"} />
     </div>
   );
 }
