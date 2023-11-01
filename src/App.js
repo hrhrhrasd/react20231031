43d53382b3e870231fa6e7c9858ce018a7ba6fca
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 12:22:43 +0900

    다른 파일의 컴포넌트 같은 파일이라면 ,로 구분 import

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import { MyElem } from "./component/MyElem";
-import { MyComp } from "./component/MyComp";
-
-//  export된 컴포넌트(변수, 함수, 객체) 를 import 해서 사용
+import {
+  MyBox,
+  MyElem,
+  MyContainer,
+  name,
+  address,
+  person,
+} from "./component/MyBox";
 
 function App() {
+  console.log(name);
+  console.log(address);
+  console.log(person);
   return (
     <>
-      <MyComp />
+      <MyBox />
       <MyElem />
+      <MyContainer />
     </>
   );
 }
--- src/component/MyComp.js
+++
@@ -1,7 +0,0 @@
-export function MyComp() {
-  return (
-    <div>
-      <p>너무너무 긴 코드</p>
-    </div>
-  );
-}
