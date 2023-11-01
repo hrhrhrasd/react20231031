62d779ffba580f84c51127ed2bc7845da8aa6c50
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 14:48:03 +0900

    여러 파일에서 중복된 named export 를 import 하는 방법 (as 사용 별칭) 연습 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import Box, { color as bColor } from "./component/Box";
+import Container, { color as cColor } from "./component/Container";
 
 function App() {
   return (
     <div>
-
+      <Box style={{ color: bColor }} />
+      <Container style={{ color: cColor }} />
     </div>
   );
 }
---
+++ src/component/Box.js
@@ -0,0 +1,9 @@
+export default function Box({ style }) {
+  return (
+    <div style={style}>
+      <h1>Box text</h1>
+    </div>
+  );
+}
+
+export const color = "red";
