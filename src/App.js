f3693597040a84aaa61455b32811ad173331d38b
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 11:13:31 +0900

    props 구조 분해 할당 연습 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-
 function App() {
   return (
     <div>
+      <MyElem
+        p={"5px"}
+        m={"5px"}
+        color={"green"}
+        bg={"yellow"}
+        text={"hello"}
+      />
+      <MyElem p={"10px"} m={"10px"} color={"red"} bg={"blue"} text={"world"} />
+    </div>
+  );
+}
 
+function MyElem({ p, m, color, bg, text }) {
+  return (
+    <div
+      style={{
+        padding: p,
+        margin: m,
+        color: color,
+        background: bg,
+      }}
+    >
+      <p>{text}</p>
     </div>
   );
 }
