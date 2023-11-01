04896e3cda9c0037683df5c76ea83ddf0befa6ff
hrhrhrasd <gmleh1245@gmail.com>
2023-11-01 10:15:47 +0900

    props 연습 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-
 function App() {
   return (
     <div>
+      <MyText
+        value={"My Text"}
+        style={{
+          color: "white",
+          background: "black",
+          width: "500px",
+          fontSize: "2rem",
+        }}
+      />
+    </div>
+  );
+}
 
+function MyText(props) {
+  return (
+    <div style={props.style}>
+      <p>{props.value}</p>
     </div>
   );
 }
