7c23db312bf10dbab11bfb136c5e0783785d7955
hrhrhrasd <gmleh1245@gmail.com>
2023-11-02 11:35:47 +0900

    chakra simpleGfid / 반응형 웹

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Flex w={"100%"} h={"100px"} bg={"gray.300"} gap={1} align={"center"}>
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"blue"}>Explicabo.</Box>
-        <Box bg={"red"}>Consectetur.</Box>
-        <Box bg={"gray"}>Dolores.</Box>
-        <Box bg={"green"}>Corporis.</Box>
-      </Flex>
+      <SimpleGrid gap={3} columns={{ base: 2, md: 3 }}>
+        <Box bg="blue.300">Lorem.</Box>
+        <Box bg="blue.300">Vel?</Box>
+        <Box bg="blue.300">Debitis.</Box>
+        <Box bg="blue.300">Eaque?</Box>
+        <Box bg="blue.300">Nam!</Box>
+        <Box bg="blue.300">Nemo.</Box>
+        <Box bg="blue.300">Ipsam!</Box>
+        <Box bg="blue.300">Dolore.</Box>
+        <Box bg="blue.300">Voluptatum.</Box>
+        <Box bg="blue.300">Unde!</Box>
+        <Box bg="blue.300">Ullam.</Box>
+        <Box bg="blue.300">Rem.</Box>
+        <Box bg="blue.300">Voluptas.</Box>
+        <Box bg="blue.300">At.</Box>
+        <Box bg="blue.300">Veritatis.</Box>
+        <Box bg="blue.300">Sed.</Box>
+        <Box bg="blue.300">Ab!</Box>
+        <Box bg="blue.300">Cum.</Box>
+        <Box bg="blue.300">Consequatur?</Box>
+        <Box bg="blue.300">Sequi?</Box>
+        <Box bg="blue.300">Ipsum!</Box>
+        <Box bg="blue.300">Libero?</Box>
+        <Box bg="blue.300">Porro?</Box>
+        <Box bg="blue.300">Odio.</Box>
+        <Box bg="blue.300">Quas!</Box>
+        <Box bg="blue.300">Voluptatibus?</Box>
+        <Box bg="blue.300">Totam!</Box>
+        <Box bg="blue.300">Unde?</Box>
+        <Box bg="blue.300">Inventore?</Box>
+        <Box bg="blue.300">Praesentium?</Box>
+      </SimpleGrid>
     </>
   );
 }
