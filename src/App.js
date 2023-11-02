e406aa2c75b37cb9c315c38a0ea81a5c9ef4940c
hrhrhrasd <gmleh1245@gmail.com>
2023-11-02 11:19:14 +0900

    cahkra Felx 태그

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 function App() {
   return (
-    <Box
-      border={"1px solid black"}
-      w={"300px"}
-      marginLeft={"50px"}
-      marginTop={"50px"}
-    >
-      <Box bg={"yellow"} borderBottom={"1px dotted black"}>
-        Lorem ipsum.
-      </Box>
-      <Box fontWeight={"bold"}>Harum, placeat!</Box>
-      <Box textAlign={"center"}>Tempore, voluptates?</Box>
-      <Box>Distinctio, tenetur!</Box>
-      <Box>Dolorem, reprehenderit?</Box>
-      <Box>Cumque, dignissimos!</Box>
-      <Box>Commodi, veritatis?</Box>
-      <Box>Aliquam, nihil.</Box>
-      <Box>Aliquid, consectetur?</Box>
-      <Box>Quidem, totam.</Box>
-      <Box>Ex, perspiciatis.</Box>
-      <Box>Quibusdam, temporibus.</Box>
-      <Box>Aliquam, ex.</Box>
-      <Box>Inventore, porro!</Box>
-      <Box>Animi, placeat!</Box>
-      <Box>Nisi, voluptate.</Box>
-      <Box>Earum, temporibus.</Box>
-      <Box>Rerum, sequi.</Box>
-      <Box>Officia, tempora?</Box>
-      <Box>Beatae, iusto.</Box>
-    </Box>
+    <>
+      <Flex w={"100%"} h={"100px"} bg={"gray.300"} gap={1} align={"center"}>
+        <Box bg={"gold"}>Lorem.</Box>
+        <Box bg={"blue"}>Explicabo.</Box>
+        <Box bg={"red"}>Consectetur.</Box>
+        <Box bg={"gray"}>Dolores.</Box>
+        <Box bg={"green"}>Corporis.</Box>
+      </Flex>
+    </>
   );
 }
 
