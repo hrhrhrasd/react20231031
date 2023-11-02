c4986a1c60950748be82b809f44966c645cb17d3
hrhrhrasd <gmleh1245@gmail.com>
2023-11-02 11:12:32 +0900

    cahkr Box 태그 연습 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <Box
+      border={"1px solid black"}
+      w={"300px"}
+      marginLeft={"50px"}
+      marginTop={"50px"}
+    >
+      <Box bg={"yellow"} borderBottom={"1px dotted black"}>
+        Lorem ipsum.
+      </Box>
+      <Box fontWeight={"bold"}>Harum, placeat!</Box>
+      <Box textAlign={"center"}>Tempore, voluptates?</Box>
+      <Box>Distinctio, tenetur!</Box>
+      <Box>Dolorem, reprehenderit?</Box>
+      <Box>Cumque, dignissimos!</Box>
+      <Box>Commodi, veritatis?</Box>
+      <Box>Aliquam, nihil.</Box>
+      <Box>Aliquid, consectetur?</Box>
+      <Box>Quidem, totam.</Box>
+      <Box>Ex, perspiciatis.</Box>
+      <Box>Quibusdam, temporibus.</Box>
+      <Box>Aliquam, ex.</Box>
+      <Box>Inventore, porro!</Box>
+      <Box>Animi, placeat!</Box>
+      <Box>Nisi, voluptate.</Box>
+      <Box>Earum, temporibus.</Box>
+      <Box>Rerum, sequi.</Box>
+      <Box>Officia, tempora?</Box>
+      <Box>Beatae, iusto.</Box>
+    </Box>
+  );
 }
 
 export default App;
