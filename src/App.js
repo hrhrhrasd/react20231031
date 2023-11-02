110db60080351b417ae9bd1613aa3777a0546daa
hrhrhrasd <gmleh1245@gmail.com>
2023-11-02 14:53:13 +0900

    chakra Input / Radio / Checkbox  사용

--- src/App.js
+++ src/App.js
@@ -1,51 +1,57 @@
-import { Button } from "@chakra-ui/react";
-import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
+import React from "react";
+import {
+  Box,
+  Center,
+  Checkbox,
+  Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Radio,
+  RadioGroup,
+} from "@chakra-ui/react";
 
-function App() {
+function App(props) {
   return (
-    <>
-      <Button colorScheme="blue">Lorem.</Button>
-      <Button colorScheme="red">Lorem.</Button>
-      <Button colorScheme="yellow">Numquam!</Button>
-      <Button colorScheme="orange">Eligendi.</Button>
-      <Button colorScheme="teal" size={"sm"}>
-        Dolorum.
-      </Button>
-      <Button colorScheme="messenger" size={"lg"}>
-        Nesciunt.
-      </Button>
-      <Button colorScheme="telegram" variant="outline">
-        Impedit?
-      </Button>
-      <Button colorScheme="orange" variant="ghost">
-        Magni.
-      </Button>
-      <Button variant="link">Quos.</Button>
-      <Button leftIcon={<EmailIcon />}>Quos!</Button>
-      <Button rightIcon={<ArrowRightIcon />}>Optio.</Button>
-      <Button isLoading>Temporibus!</Button>
-      <Button isLoading loadingText="전송중">
-        Quae.
-      </Button>
-      <Button>Dolor.</Button>
-      <Button>Debitis.</Button>
-      <Button>Pariatur!</Button>
-      <Button>Explicabo!</Button>
-      <Button>Unde.</Button>
-      <Button>Possimus!</Button>
-      <Button>Delectus?</Button>
-      <Button>Neque?</Button>
-      <Button>Facilis.</Button>
-      <Button>Quaerat.</Button>
-      <Button>Architecto?</Button>
-      <Button>Ab?</Button>
-      <Button>Autem?</Button>
-      <Button>Amet?</Button>
-      <Button>Id.</Button>
-      <Button>Corporis.</Button>
-      <Button>Omnis.</Button>
-      <Button>Eveniet?</Button>
-    </>
+    <div>
+      <Center>
+        <Box w={"480px"}>
+          <FormControl>
+            <FormLabel>이름</FormLabel>
+            <Input />
+            <FormHelperText>띄어쓰기 없이 입력</FormHelperText>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center m={"50px"}>
+        <Box w={"480px"}>
+          <FormControl>
+            <FormLabel>선택</FormLabel>
+            <RadioGroup>
+              <Flex gap={5}>
+                <Radio value={"value1"}>Lorem.</Radio>
+                <Radio value={"value2"}>Consequatur?</Radio>
+                <Radio value={"value3"}>Aliquid?</Radio>
+                <Radio value={"value4"}>Quae.</Radio>
+              </Flex>
+            </RadioGroup>
+          </FormControl>
+        </Box>
+      </Center>
+      <Center m={"50px"}>
+        <Box w={"480px"}>
+          <FormControl>
+            <FormLabel>여러개 선택</FormLabel>
+            <Flex gap={5}>
+              <Checkbox>Lorem.</Checkbox>
+              <Checkbox>Labore?</Checkbox>
+              <Checkbox>Rerum!</Checkbox>
+            </Flex>
+          </FormControl>
+        </Box>
+      </Center>
+    </div>
   );
 }
 
