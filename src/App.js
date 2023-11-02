c5191870fcf84b15aa772f9b1eb031ef274ba275
hrhrhrasd <gmleh1245@gmail.com>
2023-11-02 10:21:53 +0900

    Box 태그 연습 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Box } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <Box bgColor={"tomato"} fontSize={"20px"}>
+      <Box bgColor={"red.500"}>Lorem ipsum dolor.</Box>
+      <Box textColor={"blue"}>Lorem ipsum dolor.</Box>
+      <Box m={"10px"}>Exercitationem, odio, veniam.</Box>
+      <Box>Dignissimos nihil, voluptatibus.</Box>
+      <Box p={"10px"}>Ab, ducimus, fugiat.</Box>
+      <Box textDecoration={"underline"}>Et, quas similique?</Box>
+      <Box fontWeight={"bold"}>Aut, beatae, fugiat.</Box>
+      <Box>Facere, vel voluptas.</Box>
+      <Box>A, harum saepe!</Box>
+      <Box>Consequuntur, delectus possimus.</Box>
+      <Box>Autem, doloremque, eos.</Box>
+    </Box>
+  );
 }
 
 export default App;
