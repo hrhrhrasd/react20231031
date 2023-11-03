05cbb9ac788233204322dc915820868831051e44
hrhrhrasd <gmleh1245@gmail.com>
2023-11-03 12:33:20 +0900

    event.preventDefault() : 기본기능 취소 연습 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 function App() {
-  return <></>;
+  function handleClick(e) {
+    e.preventDefault();
+    window.location.href = "https://www.naver.com";
+  }
+
+  return (
+    <>
+      <a href="https://www.daum.net" onClick={handleClick}>
+        daum 이동
+      </a>
+    </>
+  );
 }
 
 export default App;
