55130dd196975a2b581bbd6afb875acb6a9db2a0
hrhrhrasd <gmleh1245@gmail.com>
2023-11-03 09:33:22 +0900

    children prop 연습 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import * as PropTypes from "prop-types";
+import { Box, Button, FormControl } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <FormControl>
+      <Button as={"a"} href={to}>
+        {children}
+      </Button>
+    </FormControl>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to={"https://www.naver.com"}>to naver</MyLink>
+      <MyLink to={"https://www.daum.net"}>to daum</MyLink>
+    </>
+  );
 }
 
 export default App;
