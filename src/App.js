fc0f4b5e24f60a59b371abd3c0fe1877db0aa22c
hrhrhrasd <gmleh1245@gmail.com>
2023-11-03 14:25:54 +0900

    event.stopPropagation() >> bubbling 방지 연습1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+import { Button, Center } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  function handleOuterBoxClick(e) {
+    e.stopPropagation();
+    console.log("바깥 상자 클릭");
+  }
+  function handleInnerBoxClick(e) {
+    e.stopPropagation();
+    console.log("내부 상자 클릭");
+  }
+  function handleButtonClick(e) {
+    e.stopPropagation();
+    console.log("버튼 클릭");
+  }
+
+  return (
+    <>
+      <Center onClick={handleOuterBoxClick} w={"200px"} h={"200px"} bg={"aqua"}>
+        <Center
+          onClick={handleInnerBoxClick}
+          w={"100px"}
+          h={"100px"}
+          bg={"sandybrown"}
+        >
+          <Button onClick={handleButtonClick}>button</Button>
+        </Center>
+      </Center>
+    </>
+  );
 }
 
 export default App;
