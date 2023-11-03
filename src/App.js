ef00a28bb187f5da714b40235c2905363349dd61
hrhrhrasd <gmleh1245@gmail.com>
2023-11-03 15:46:46 +0900

    useState 연습1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  let result = number1 + number2;
+
+  function handleChange1(e) {
+    setNumber1(Number(e.target.value));
+  }
+
+  function handleChange2(e) {
+    setNumber2(Number(e.target.value));
+  }
+
+  return (
+    <>
+      <input type="number" onChange={handleChange1} /> +
+      <input type="number" onChange={handleChange2} />= {result}
+    </>
+  );
 }
 
 export default App;
