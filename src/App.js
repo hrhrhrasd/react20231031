5c801415bb81b2cb4965981d426cc064dbdb9ff0
hrhrhrasd <gmleh1245@gmail.com>
2023-11-03 11:44:37 +0900

    이벤트 핸들러 메소드에 event객체가 전달된다 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { Button } from "@chakra-ui/react";
+import { Input } from "@chakra-ui/react";
 
 function App(props) {
-  function hadleClick(e) {
-    // 브라우저는 이벤트 핸들러 메소드에
-    // event 객체를 파라미터로 넣어줌
-    // console.log(e);
-    console.log(e.target);
+  function handleChange(e) {
+    console.log(e.target.value);
   }
   return (
     <div>
-      <Button onClick={hadleClick}>button1</Button>
-      <Button onClick={hadleClick}>button2</Button>
+      <Input onChange={handleChange} />
+      <Input onChange={handleChange} />
     </div>
   );
 }
