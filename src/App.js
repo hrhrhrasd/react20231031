c2bdbad0d0dc7f3c8949313dc511aed3d0149754
hrhrhrasd <gmleh1245@gmail.com>
2023-11-03 10:49:36 +0900

    arrow function 연습 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  return (
+    <div>
+      <Button onClick={() => console.log("first")}>Lorem.</Button>
+      <Button onClick={() => console.log("second")}>Beatae!</Button>
+    </div>
+  );
 }
 
 export default App;
