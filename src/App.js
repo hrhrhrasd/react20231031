d1776797335b00ea9e686d12ad1cb5744b2980ad
hrhrhrasd <gmleh1245@gmail.com>
2023-11-06 17:48:26 +0900

    use immer 로 상태 변경하기 연습 todolist

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
 import React from "react";
+import { useImmer } from "use-immer";
+import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
 
+let nextId = 0;
 function App(props) {
-  return <div></div>;
+  const [items, updateItems] = useImmer([]);
+
+  function handleButtonClick(todo) {
+    updateItems((draft) => {
+      draft.push({ id: nextId, done: false, text: todo });
+      nextId++;
+      console.log(nextId);
+    });
+  }
+
+  function handleDoneButtonClick(id) {
+    updateItems((draft) => {
+      for (let i = 0; i < draft.length; i++) {
+        draft[i].done = draft[i].id == id ? !draft[i].done : draft[i].done;
+      }
+    });
+  }
+
+  function handleRemoveButtonClick(id) {
+    updateItems((draft) => {
+      for (let i = 0; i < draft.length; i++) {
+        if (draft[i].id == id) {
+          draft.splice(i, 1);
+          break;
+        }
+      }
+    });
+  }
+
+  return (
+    <div>
+      <Button onClick={() => handleButtonClick("자바공부")}>자바공부</Button>
+      <Button onClick={() => handleButtonClick("점심먹기")}>점심먹기</Button>
+      <Button onClick={() => handleButtonClick("잠자기")}>잠자기</Button>
+
+      <Box>
+        <UnorderedList>
+          {items.map((item) => (
+            <ListItem
+              key={item.id}
+              textDecoration={item.done ? "line-through" : "none"}
+            >
+              {item.text}
+              <Button onClick={() => handleDoneButtonClick(item.id)}>
+                완료
+              </Button>
+              <Button onClick={() => handleRemoveButtonClick(item.id)}>
+                삭제
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
