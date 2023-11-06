a7fcf087792598ef385ae5a6ed8c47081066f630
hrhrhrasd <gmleh1245@gmail.com>
2023-11-06 15:14:25 +0900

    배열인 상태를 복사해서 상태변경 remove 하기 연습 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
-function App() {
-  return <></>;
+import React, { useState } from "react";
+import { Box, Button, Input, ListItem, UnorderedList } from "@chakra-ui/react";
+
+function App(props) {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick() {
+    setItems([...items, text]);
+  }
+
+  function handleRemoveButtonClick(index) {
+    setItems([...items].filter((item, i) => i !== index));
+  }
+
+  return (
+    <div>
+      <Box>
+        <Input value={text} onChange={(e) => setText(e.target.value)}></Input>
+        <Button onClick={handleButtonClick}>추가</Button>
+      </Box>
+      <Box>
+        <UnorderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>
+              {item}
+              <Button onClick={() => handleRemoveButtonClick(index)}>
+                지우기
+              </Button>
+            </ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
