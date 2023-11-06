f6ff0afe224fda6a205258635ab428f321e212b5
hrhrhrasd <gmleh1245@gmail.com>
2023-11-06 16:25:33 +0900

    use immer 로 상태 변경하기 (깊은 복사)

--- src/App.js
+++ src/App.js
@@ -1,50 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
 import { useImmer } from "use-immer";
 import { Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [person, setPerson] = useState({ name: "son", city: "seoul" });
-  const [user, updateUser] = useImmer({ name: "lee", city: "busan" });
+  const [person, updatePerson] = useImmer({
+    name: "son",
+    address: {
+      city: "seoul",
+      country: "korea",
+    },
+  });
 
-  function hadleNameChange(e) {
-    const newPerson = { ...person };
-    newPerson.name = e.target.value;
-    setPerson(newPerson);
-  }
-
-  function hadleCityChange(e) {
-    const newPerson = { ...person };
-    newPerson.city = e.target.value;
-    setPerson(newPerson);
-  }
-
-  function hadleUserNameChange(e) {
-    updateUser((draft) => {
-      // 리턴 x
+  function handleNameChange(e) {
+    updatePerson((draft) => {
       draft.name = e.target.value;
     });
   }
 
-  function hadleUserCityChange(e) {
-    updateUser((draft) => {
-      draft.city = e.target.value;
+  function handleCountryChange(e) {
+    updatePerson((draft) => {
+      draft.address.country = e.target.value;
+    });
+  }
+
+  function handleCityChange(e) {
+    updatePerson((draft) => {
+      draft.address.city = e.target.value;
     });
   }
 
   return (
     <div>
-      <Input value={person.name} onChange={hadleNameChange} />
-      <Input value={person.city} onChange={hadleCityChange} />
+      <Input value={person.name} onChange={handleNameChange} />
+      <Input value={person.address.country} onChange={handleCountryChange} />
+      <Input value={person.address.city} onChange={handleCityChange} />
       <Text>
-        {person.name} = {person.city} 거주
-      </Text>
-
-      <hr />
-
-      <Input value={user.name} onChange={hadleUserNameChange} />
-      <Input value={user.city} onChange={hadleUserCityChange} />
-      <Text>
-        {user.name} = {user.city} 거주
+        {person.name} 은 {person.address.country}, {person.address.city} 에 거주
       </Text>
     </div>
   );
