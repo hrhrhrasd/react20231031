04f57f646b2a293046e843efe689062ca1f9cf2d
hrhrhrasd <gmleh1245@gmail.com>
2023-11-06 15:51:28 +0900

    use immer 로 상태 변경하기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { useImmer } from "use-immer";
+import { Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  return <div></div>;
+  const [person, setPerson] = useState({ name: "son", city: "seoul" });
+  const [user, updateUser] = useImmer({ name: "lee", city: "busan" });
+
+  function hadleNameChange(e) {
+    const newPerson = { ...person };
+    newPerson.name = e.target.value;
+    setPerson(newPerson);
+  }
+
+  function hadleCityChange(e) {
+    const newPerson = { ...person };
+    newPerson.city = e.target.value;
+    setPerson(newPerson);
+  }
+
+  function hadleUserNameChange(e) {
+    updateUser((draft) => {
+      // 리턴 x
+      draft.name = e.target.value;
+    });
+  }
+
+  function hadleUserCityChange(e) {
+    updateUser((draft) => {
+      draft.city = e.target.value;
+    });
+  }
+
+  return (
+    <div>
+      <Input value={person.name} onChange={hadleNameChange} />
+      <Input value={person.city} onChange={hadleCityChange} />
+      <Text>
+        {person.name} = {person.city} 거주
+      </Text>
+
+      <hr />
+
+      <Input value={user.name} onChange={hadleUserNameChange} />
+      <Input value={user.city} onChange={hadleUserCityChange} />
+      <Text>
+        {user.name} = {user.city} 거주
+      </Text>
+    </div>
+  );
 }
 
 export default App;
