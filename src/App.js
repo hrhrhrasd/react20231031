f67c39ae31230d58fb918e064703b92a7c884bd1
hrhrhrasd <gmleh1245@gmail.com>
2023-11-06 09:38:06 +0900

    use state 연습 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <></>;
+import React, { useState } from "react";
+import { Button } from "@chakra-ui/react";
+
+function App(props) {
+  const [number, setNumber] = useState(0);
+
+  return (
+    <div>
+      <h1>{number}</h1>
+      {/* eslint-disable-next-line react/jsx-no-undef */}
+      <Button
+        onClick={() => {
+          setNumber(number + 1);
+        }}
+      >
+        증가
+      </Button>
+
+      <Button
+        onClick={() => {
+          setNumber(number - 1);
+        }}
+      >
+        감소
+      </Button>
+    </div>
+  );
 }
 
 export default App;
