834f87b1b2ca41e44cf3858ae7d162eb2c23fa42
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 14:01:35 +0900

    context 연습

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { useState } from "react";
-import { Input, Text } from "@chakra-ui/react";
-import { m } from "framer-motion";
+import React, { createContext, useContext, useState } from "react";
+import { Button, Text } from "@chakra-ui/react";
 
-function CComp({ message }) {
-  return <Text>{message}</Text>;
+function CComp() {
+  const message = useContext(MessageContext);
+  return <Text>받은 메시지 : {message}</Text>;
 }
 
-function AComp({ message }) {
-  return <CComp message={message} />;
+function BComp() {
+  return <CComp />;
 }
 
-function DComp({ onTextChage }) {
-  return <Input onChange={(e) => onTextChage(e.target.value)} />;
-}
-
-function BComp({ onTextChange }) {
-  return <DComp onTextChage={onTextChange} />;
+function AComp() {
+  return <BComp />;
 }
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [message, setMessage] = useState("첫 메시지");
+  // message state를 CComp에 전달하기
+  // 1. context 만들기 : createContext();
+  // 2. context에 state 넣기 : <Context.Provider value={state} />
+  // 3. tree안에 사용하기 : useContext(Context);
+
   return (
     <div>
-      <AComp message={message} />
-      <BComp onTextChange={setMessage} />
+      <Button onClick={() => setMessage("바뀐 메시지")}>button</Button>
+      <MessageContext.Provider value={message}>
+        <AComp />
+      </MessageContext.Provider>
     </div>
   );
 }
+
+// context 이름 : ...Context  대문자로 시작
+const MessageContext = createContext(null);
 
 export default App;
