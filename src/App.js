cf311e3afb1dd8acb890ba9f26f3185effcaec03
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 14:14:45 +0900

    context value 에 state, setState 를 객체로 넣기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { Button, Text } from "@chakra-ui/react";
 
-function ChildComp({ onClick }) {
-  return <Button onClick={() => onClick("다른 메시지")}>button</Button>;
+function AComp() {
+  const value = useContext(MessageContext);
+  return (
+    <Button onClick={() => value.setMessage("바뀐 메시지")}>
+      {" "}
+      메시지 변경하기{" "}
+    </Button>
+  );
 }
 
-function SomeComp({ onClick }) {
-  return <ChildComp onClick={onClick} />;
-}
-
-function OtherChildComp({ message }) {
+function BComp() {
+  // const value = useContext(MessageContext);
+  // return <Text>{value.message}</Text>;
+  const { message } = useContext(MessageContext);
   return <Text>{message}</Text>;
 }
 
-function OtherComp({ message }) {
-  return <OtherChildComp message={message} />;
-}
-
 function App(props) {
-  const [message, setMessage] = useState("첫 메시지");
+  const [message, setMessage] = useState("초기 상태 메시지");
 
   return (
     <div>
-      <SomeComp onClick={setMessage} />
-      <OtherComp message={message} />
+      {/* 2. context에 value 넣기*/}
+      <MessageContext.Provider value={{ message, setMessage }}>
+        <AComp />
+        <BComp />
+      </MessageContext.Provider>
     </div>
   );
 }
+// 1. context 만들기
+const MessageContext = createContext(null);
 
 export default App;
