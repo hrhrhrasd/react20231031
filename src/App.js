2cf7e52550a86fc15ebfd082cfa56d984b9ee4dc
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 11:15:11 +0900

    다른 컴포넌트의 상태 바꾸기 : 공통 부모에게 상태를 두고 사용해야 함 연습 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import * as PropTypes from "prop-types";
+import { Box, Input, Text } from "@chakra-ui/react";
+
+function MyInput({ address, onChangeText }) {
+  return (
+    <div>
+      <Input
+        value={address}
+        onChange={(e) => {
+          onChangeText(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+function MyText({ address }) {
+  return (
+    <Box>
+      <Text>{address}</Text>
+    </Box>
+  );
+}
 
 function App(props) {
-  return <div></div>;
+  const [address, setAddress] = useState("");
+
+  return (
+    <div>
+      <MyInput
+        address={address}
+        onChangeText={(text) => {
+          setAddress(text);
+        }}
+      />
+      <MyText address={address} />
+    </div>
+  );
 }
 
 export default App;
