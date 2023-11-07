71634b6cb7932f9874e21a3e93db0dbd86e363cc
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 12:16:07 +0900

    lift state up, prop drilling 예제 연습1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Input, Text } from "@chakra-ui/react";
+import { m } from "framer-motion";
+
+function CComp({ message }) {
+  return <Text>{message}</Text>;
+}
+
+function AComp({ message }) {
+  return <CComp message={message} />;
+}
+
+function DComp({ onTextChage }) {
+  return <Input onChange={(e) => onTextChage(e.target.value)} />;
+}
+
+function BComp({ onTextChange }) {
+  return <DComp onTextChage={onTextChange} />;
+}
 
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState("");
+  return (
+    <div>
+      <AComp message={message} />
+      <BComp onTextChange={setMessage} />
+    </div>
+  );
 }
 
 export default App;
