015cfa6547304cdbc65807cb51f5da06f1d43c19
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 16:38:03 +0900

    ajax get 사용 state 업데이트 연습 2 (객체상태로 가져오기)

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function App(props) {
-  const [message, setMessage] = useState("");
+  const [customer, setCustomer] = useState({});
   useEffect(() => {
     axios
-      .get("/api/main1/sub2")
+      .get("/api/main1/sub3")
       .then((response) => response.data)
-      .then((data) => setMessage(data))
+      .then((data) => setCustomer(data))
       .catch((error) => console.log(error));
   }, []);
 
   return (
     <div>
-      <h1>{message}</h1>
+      <h1>{customer.customerName}</h1>
+      <h1>{customer.address}</h1>
+      <h1>{customer.city}</h1>
+      <h1>{customer.customerID}</h1>
     </div>
   );
 }
