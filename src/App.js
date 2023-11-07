af572fb7fd1eb27391814b8b8df15131ce99ad59
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 14:34:26 +0900

    context value 에 state, setState 를 객체로 넣기 연습 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+import { Input, Text } from "@chakra-ui/react";
+
+function MyInput() {
+  const { setText } = useContext(TextContext);
+  return <Input onChange={(e) => setText(e.target.value)} />;
+}
+
+function MyText() {
+  const { text } = useContext(TextContext);
+  return <Text>{text}</Text>;
+}
 
 function App(props) {
-  return <div></div>;
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <TextContext.Provider value={{ text, setText }}>
+        <MyInput />
+        <MyText />
+      </TextContext.Provider>
+    </div>
+  );
 }
+const TextContext = createContext(null);
 
 export default App;
