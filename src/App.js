0ad17611ccd879eedc57514c47fc76d5652e1991
hrhrhrasd <gmleh1245@gmail.com>
2023-11-07 16:30:34 +0900

    ajax get 사용 state 업데이트 연습 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState({});
+  useEffect(() => {
+    axios
+      .get("/api/main1/sub2")
+      .then((response) => response.data)
+      .then((data) => setMessage(data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
 }
 
 export default App;
